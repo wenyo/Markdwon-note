fd0d72e71e470fb8296771002092f419be5881c3
wen yo <wenda897436@gmail.com>
2021-02-15 23:20:59 +0800

    Starr & Trash

--- src/components/directory/Tool.tsx
+++ src/components/directory/Tool.tsx
@@ -1,16 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import NoteContext from '../../context/NoteContext';
 
 const Tool = () => {
-    const { iData, vData, vStarr, setvStarr } = useContext(NoteContext);
+    const { iData, vData, setvData } = useContext(NoteContext);
 
     const clickStar = () => {
-        let vNewStart = JSON.parse(JSON.stringify(vStarr));
-        for (const vStart of vNewStart) {
-            
-        }
+        let vNewData = JSON.parse(JSON.stringify(vData));
+        vNewData[iData].starred = !vNewData[iData].starred;
+        setvData(vNewData);
+    };
+
+    const clickTrash = () => {
+        let vNewData = JSON.parse(JSON.stringify(vData));
+        vNewData[iData].trash = !vNewData[iData].trash;
+        vNewData[iData].starred = false;
+        setvData(vNewData);
+    };
+
+    const clickAdd = () => {
+        // let vNewData = JSON.parse(JSON.stringify(vData));
+        // vNewData[vNewData.length] = {
+        //     name: 'Untitle note',
+        //     collection: 1,
+        //     starred: false,
+        //     trash: true,
+        //     updated_date: '2021/02/07 13:45',
+        //     created_date: '2021/02/01 11:00',
+        //     content: `# Untitle note`,
+        // }
     };
 
     return (
@@ -19,9 +38,9 @@ const Tool = () => {
                 <FontAwesomeIcon icon={faGithub} />
             </div>
             <div>
-                <i className="icon-delete"></i>
-                <i className="icon-start"></i>
-                <i className="icon-add"></i>
+                <i className="icon-delete" onClick={clickTrash}></i>
+                <i className="icon-start" onClick={clickStar}></i>
+                <i className="icon-add" onClick={clickAdd}></i>
             </div>
         </div>
     );
